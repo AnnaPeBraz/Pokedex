ac9e73ac2d3732b033f32e157a9b0e947df0d2c7
Anna Paula jesus braz <annap.0103@gmail.com>
2023-04-04 13:06:49 +0000

    Update searchBar.js (it is working)

--- src/Components/searchBar.js
+++ src/Components/searchBar.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 const SearchBar = () => {
+  const [search, setSearch] = useState("pikachu")  
+  const onChangeHandler = (e) => {
+    console.log("pokemon: ", e.target.value)
+    setSearch(e.target.value)
+  }
+  const onButtonClickHandler = () => {
+    console.log("O pokemon: ", search)
+  }
+  
   return (
     <div className="searchBar-container">
       <div className="searchBar">
-        <input placeholder="buscar pkm" />
+        <input placeholder="buscar pkm" onChange={onChangeHandler}/>
+        {/*{search}*/}  
+      </div>
+      <div className="searchBarButton">
+        <button onClick = {onButtonClickHandler}> Buscar </button>
       </div>
     </div>
   );
